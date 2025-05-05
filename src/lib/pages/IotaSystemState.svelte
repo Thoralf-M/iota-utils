5cf34dec787331a5b35abed01ccbd0f113728fd4
Thoralf Müller <thoralf.mue@gmail.com>
2025-05-05 11:25:41 +0200

    feat(IotaSystemState): add candidate and pending validators

--- src/lib/pages/IotaSystemState.svelte
+++ src/lib/pages/IotaSystemState.svelte
@@ -16,6 +16,113 @@
             console.error(err);
         }
     };
+    let showAllValidatorData = false;
+    const getCandidateValidators = async () => {
+        try {
+            let client = await getClient();
+            apiVersion = (await client.getRpcApiVersion()) || "";
+            const systemState = await client.getLatestIotaSystemState();
+
+            const validatorCandidatesId = systemState.validatorCandidatesId;
+            // Note: pagination is not handled here, so only the first page of results will be returned
+            const candidateValidators = await client.getDynamicFields({
+                parentId: validatorCandidatesId,
+            });
+            let validatorCandidates = [];
+            for (const candidateValidator of candidateValidators.data) {
+                const validatorWrapper = await client.getDynamicFieldObject({
+                    parentId: validatorCandidatesId,
+                    name: candidateValidator.name,
+                });
+                const validatorV1 = await client.getDynamicFields({
+                    parentId:
+                        // @ts-ignore
+                        validatorWrapper.data?.content.fields.value.fields.inner
+                            .fields.id.id,
+                });
+                const validatorObject = await client.getObject({
+                    id: validatorV1.data[0].objectId,
+                    options: { showContent: true },
+                });
+
+                const validator =
+                    // @ts-ignore
+                    validatorObject.data?.content.fields.value.fields;
+                if (!showAllValidatorData) {
+                    cleanupValidatorFields(validator);
+                }
+                validatorCandidates.push(validator);
+                value = validatorCandidates;
+            }
+            if (validatorCandidates.length == 0) {
+                value = "No candidate validators";
+            }
+        } catch (err: any) {
+            value = err.toString();
+            console.error(err);
+        }
+    };
+    const getPendingValidators = async () => {
+        try {
+            let client = await getClient();
+            apiVersion = (await client.getRpcApiVersion()) || "";
+            const systemState = await client.getLatestIotaSystemState();
+            const pendingActiveValidatorsId =
+                systemState.pendingActiveValidatorsId;
+
+            // Note: pagination is not handled here, so only the first page of results will be returned
+            const pendingValidators = await client.getDynamicFields({
+                parentId: pendingActiveValidatorsId,
+            });
+            let validatorsPending = [];
+            for (const pendingValidator of pendingValidators.data) {
+                const validatorObject = await client.getObject({
+                    id: pendingValidator.objectId,
+                    options: { showContent: true },
+                });
+
+                const validator =
+                    // @ts-ignore
+                    validatorObject.data?.content.fields.value.fields;
+                if (!showAllValidatorData) {
+                    cleanupValidatorFields(validator);
+                }
+                validatorsPending.push(validator);
+                value = validatorsPending;
+            }
+            if (validatorsPending.length == 0) {
+                value = "No pending validators";
+            }
+        } catch (err: any) {
+            value = err.toString();
+            console.error(err);
+        }
+    };
+    // Remove fields from the validator to have a cleaner output
+    function cleanupValidatorFields(validator: any) {
+        delete validator.extra_fields;
+        delete validator.metadata.type;
+        delete validator.metadata.fields.authority_pubkey_bytes;
+        delete validator.metadata.fields.next_epoch_authority_pubkey_bytes;
+        delete validator.metadata.fields.next_epoch_net_address;
+        delete validator.metadata.fields.next_epoch_network_pubkey_bytes;
+        delete validator.metadata.fields.next_epoch_p2p_address;
+        delete validator.metadata.fields.next_epoch_primary_address;
+        delete validator.metadata.fields.next_epoch_proof_of_possession;
+        delete validator.metadata.fields.next_epoch_protocol_pubkey_bytes;
+        delete validator.metadata.fields.net_address;
+        delete validator.metadata.fields.p2p_address;
+        delete validator.metadata.fields.primary_address;
+        delete validator.metadata.fields.image_url;
+        delete validator.metadata.fields.extra_fields;
+        delete validator.metadata.fields.network_pubkey_bytes;
+        delete validator.metadata.fields.proof_of_possession;
+        delete validator.metadata.fields.protocol_pubkey_bytes;
+        delete validator.staking_pool.type;
+        delete validator.staking_pool.fields.exchange_rates;
+        delete validator.staking_pool.fields.extra_fields;
+        delete validator.staking_pool.fields.id;
+    }
     let showJsonTree = true;
 </script>
 
@@ -23,6 +130,17 @@
     <button on:click={() => getLatestSystemState()}>
         get latest iota system state
     </button>
+    <button on:click={() => getCandidateValidators()}>
+        candidate validators
+    </button>
+    <button on:click={() => getPendingValidators()}>
+        pending validators
+    </button>
+    show full data (set before requesting):
+    <select bind:value={showAllValidatorData}>
+        <option value={true}>{true}</option>
+        <option value={false}>{false}</option>
+    </select>
 
     <div class="value" hidden={Object.keys(value).length == 0}>
         <div>
