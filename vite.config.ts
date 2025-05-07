4e6eb057e3239a37a03d609b83d600e0e658030f
Thoralf Müller <thoralf.mue@gmail.com>
2025-05-07 22:39:38 +0200

    feat(LedgerNano): make pnpm dev work

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 import inject from '@rollup/plugin-inject';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [svelte(),
     // Required for the HID connection for Ledger devices
+    NodeGlobalsPolyfillPlugin({
+        buffer: true
+    }),
     inject({
         Buffer: ['buffer', 'Buffer'],
     })],
-
+    resolve: {
+        alias: {
+            buffer: 'buffer/',
+        }
+    },
 })
